b6726c48897b400c6737632766cb6e7896876b00
martins-aline <contatomartinsaline@gmail.com>
2022-10-26 21:04:06 -0300

    adiciona pasta de lógica

---
+++ LógicaDeProgramação/ER.js
@@ -0,0 +1,26 @@
+/* let peso = 50
+
+if (peso < 100) {
+    console.log("A peça deve ter no mínimo 100g")
+} else {
+    console.log("A peça possui tamanho adequado")
+}
+
+
+var listaDePecas = ["Lanterna", "Motor", "amortecedor", "cambio", "painel", "cabeçote"]
+
+if (listaDePecas.length > 10) {
+    //é possível cadastrar mais!!
+    console.log("É possível cadastrar mais peças.")
+} else {
+    console.log("não é possível cadastrar mais.")
+}
+
+
+let nomeDaPeca = "Lanterna"
+
+if (nomeDaPeca.length > 3) {
+    console.log("Siga com o cadastro")
+} else {
+    console.log("O nome da peça possui menos que três caracteres, por isso não pode ser cadastrado")
+} */
